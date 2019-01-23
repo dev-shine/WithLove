ed8448a0a2bdf2ec74d0d6d98052c3f37df659b5
robbieve <robbieeve751994@gmail.com>
2019-01-23 03:08:26 -0500

    implement login and signup

---
+++ app/screens/LogIn/index.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Image
+} from 'react-native';
+import styles from './styles';
+
+export default class Login extends Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.inputContainer}>
+          <Image style={[styles.icon, styles.inputIcon]} source={{uri: 'https://png.icons8.com/password/androidL/40/3498db'}}/>
+          <TextInput style={styles.inputs}
+              placeholder="Email"
+              keyboardType="email-address"
+              underlineColorAndroid='transparent'/>
+        </View>
+        
+        <View style={styles.inputContainer}>
+          <Image style={[styles.icon, styles.inputIcon]} source={{uri: 'https://png.icons8.com/envelope/androidL/40/3498db'}}/>
+          <TextInput style={styles.inputs}
+              placeholder="Password"
+              secureTextEntry={true}
+              underlineColorAndroid='transparent'/>
+        </View>
+     
+        <TouchableOpacity style={styles.restoreButtonContainer}>
+            <Text>Forgot?</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={[styles.buttonContainer, styles.loginButton]}>
+          <Text style={styles.loginText}>Login</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.buttonContainer}>
+            <Text>Register</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={[styles.buttonContainer, styles.fabookButton]}>
+          <View style={styles.socialButtonContent}>
+            <Image style={styles.icon} source={{uri: 'https://png.icons8.com/twitter/androidL/40/FFFFFF'}}/>
+            <Text style={styles.loginText}>Continue with twitter</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={[styles.buttonContainer, styles.googleButton]}>
+          <View style={styles.socialButtonContent}>
+            <Image style={styles.icon} source={{uri: 'https://png.icons8.com/instagram/androidL/40/FFFFFF'}}/>
+            <Text style={styles.loginText}>Sign in with instagram</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
